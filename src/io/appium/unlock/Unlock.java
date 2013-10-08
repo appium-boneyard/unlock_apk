4cafbe782dbc5caeb22a56e2fc6692f2d2e06f28
filmaj <maj.fil@gmail.com>
2013-10-08 10:34:56 -0700

    Adding an extra permission + KeyguardManager code seems to work for Samsung S3 + Samsung Note2

--- src/io/appium/unlock/Unlock.java
+++ src/io/appium/unlock/Unlock.java
@@ -1,10 +1,9 @@
 package io.appium.unlock;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
-import android.os.Handler;
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.view.*;
 
 public class Unlock extends Activity
 {
@@ -12,11 +11,19 @@ public class Unlock extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        // Set window flags to unlock screen. This works on most devices by itself.
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
+        // On most other devices, using the KeyguardManager + the permission in 
+        // AndroidManifest.xml will do the trick
+        KeyguardManager mKeyGuardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        KeyguardManager.KeyguardLock mLock = mKeyGuardManager.newKeyguardLock("Unlock");
+        mLock.disableKeyguard();
+
+        // Close yourself!
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
