6839334dc83593dd5f43398ca8d71f6d9f984f7e
汪啸 <wangxiao@changba.com>
2016-05-26 12:00:29 +0800

    add code to wake up screen before disable keyguard

--- src/io/appium/unlock/Unlock.java
+++ src/io/appium/unlock/Unlock.java
@@ -2,7 +2,9 @@ package io.appium.unlock;
 
 import android.app.Activity;
 import android.app.KeyguardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -14,14 +16,27 @@ public class Unlock extends Activity
         super.onCreate(savedInstanceState);
         // Set window flags to unlock screen. This works on most devices by itself.
         Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+        // some devices needs waking up screen first before disable keyguard
+        PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock screenLock = powerManager.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.SCREEN_DIM_WAKE_LOCK, "");
+        screenLock.acquire();
+
+        if (screenLock.isHeld()) {
+            screenLock.release();
+        }
 
         // On most other devices, using the KeyguardManager + the permission in
         // AndroidManifest.xml will do the trick
         KeyguardManager mKeyGuardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
-        KeyguardManager.KeyguardLock mLock = mKeyGuardManager.newKeyguardLock("Unlock");
-        mLock.disableKeyguard();
+        if (mKeyGuardManager.inKeyguardRestrictedInputMode()) {
+            KeyguardManager.KeyguardLock keyguardLock = mKeyGuardManager.newKeyguardLock("Unlock");
+            keyguardLock.disableKeyguard();
+        }
+
     }
 
     @Override
